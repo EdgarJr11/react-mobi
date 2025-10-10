914011fc66ae6ac5594bc69fafb354c12bee1265
EdgarJr11 <edgar.junior@mobimark.com.br>
2025-10-10 13:39:12 +0100

    Add tela home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { useState } from 'react'
+const Home = () => {
 
-export const Home = () => {
+const listagem = async () =>{
+
+const [list, setList] = useState()
+  
+  try{
+    const list = await api.get('https://apiteste.mobieduca.me/api/escolas')
+    listagem()
+
+  }
+  catch{
+
+  }
+}
+
   return (
-    <div>Home</div>
+    <div>
+      <form>
+        <input type="text" />
+        <button>
+          Buscar
+        </button>
+      </form>
+    </div>
   )
 }
+
+export default Home
